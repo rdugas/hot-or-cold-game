7922df1b7f5ea2e71794119200072e91e2543743
Rob Dugas <rob.dugas@gmail.com>
2015-01-05 23:59:27 -0600

    add new game behavior.  Fix on click action on page load problem.  Add random number gen

--- js/app.js
+++ js/app.js
@@ -2,32 +2,43 @@
 $(document).ready(function(){
 	
 	//question - do this here or in the game function?
-	
+
 	//variables to track
 	//secretNumber
 	//guess
 	//lastGuess
 	//numberOfGuesses
 
+	newGame();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
-
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
 	//on new game button click start new game
-  	$("a.new").click(newGameButton());
+	//this way did not work - fired on page load
+  	//$("a.newGame").click( newGameButton());
+
+  	//had to do this way otherwise it was firing on page load
+  	$("a.newGame").click( function()
+  		{
+  			newGameButton();
+  		});
 
 
   	function newGame(){
   		//need global scope outside of functoin
   		//use var or not?
   		//setup new game when button or page load
+  		console.log("newGame called");
+  		secretNumber = generateSecretNumber();
+  		console.log(secretNumber);
   	}
 
   	function isValidEntry(guessedNumber){
@@ -39,6 +50,7 @@ $(document).ready(function(){
   	{
   		//random bw 1-100 return number
   		//return global scope variable
+  		return Math.floor(Math.random() * 100 + 1);
   	}
 
   	function isCorrectGuess(guessedNumber){
@@ -78,14 +90,14 @@ $(document).ready(function(){
   	}
 
   	function newGameButton(){
+  		
+  		console.log("newGameButtonCalled");
   		areYouSure = confirm("Are you sure you want to start a new game?");
   		if (areYouSure) {
   			newGame();	
   		}
   	}
   	
-
-
 });
 
 
